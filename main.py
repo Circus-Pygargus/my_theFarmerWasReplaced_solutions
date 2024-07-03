a93dedbe99162fceaf18017e83e9836fafe7be3f
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-03 17:01:17 +0200

    add another loop as gameboard has now x and y axis

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from __builtins__ import *
 clear()
 
 while True:
-    if can_harvest():
-        harvest()
-        plant(Entities.Bush)
+    for i in range(get_world_size()):
+        if can_harvest():
+            harvest()
+            plant(Entities.Bush)
 
-    move(North)
+        move(North)
+
+    move(East)
