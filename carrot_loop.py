91e117364b0fddad8f45a820807dfbefa9e10d78
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-03 23:05:25 +0200

    add carrot_loop function

---
+++ carrot_loop.py
@@ -0,0 +1,32 @@
+from __builtins__ import *
+
+def carrot_loop():
+    current_loop = 0
+    wanted_loops = get_world_size()**2
+    needed_seeds_nb = get_world_size()**2
+
+    while current_loop <= wanted_loops:
+        if num_items(Items.Carrot_Seed) < needed_seeds_nb:
+            if num_items(Items.Hay) < needed_seeds_nb:
+                hay_loop()
+            elif num_items(Items.Wood) < needed_seeds_nb:
+                wood_loop()
+            else:
+                for i in range(needed_seeds_nb):
+                    trade(Items.Carrot_Seed)
+
+        for i in range(get_world_size()):
+            if can_harvest():
+                harvest()
+
+            if get_ground_type() != Grounds.Soil:
+                till()
+
+            plant(Entities.Carrots)
+
+            move(North)
+
+        move(East)
+
+        if get_pos_x() == 0:
+            current_loop += 1
