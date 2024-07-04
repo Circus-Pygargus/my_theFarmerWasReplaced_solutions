a84d18d8d55e9a240b6658982997dc284dacbd7e
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-04 23:51:44 +0200

    refacto carrot loop

--- carrot_loop.py
+++ carrot_loop.py
@@ -7,17 +7,19 @@ def carrot_loop():
     needed_fertilizer = get_world_size()**2
 
     while current_loop <= wanted_loops:
-        if get_pos_x() == 0 and num_items(Items.Carrot_Seed) < needed_seeds_nb:
-            if num_items(Items.Hay) < needed_seeds_nb:
-                hay_loop()
-            elif num_items(Items.Wood) < needed_seeds_nb:
-                wood_loop()
-            else:
-                trade(Items.Carrot_Seed, needed_seeds_nb)
-        if get_pos_x() == 0 and  num_items(Items.Fertilizer) < needed_fertilizer:
-            if 10 * num_items(Items.Pumpkin) < needed_fertilizer:
-                pumpkin_loop()
-            trade(Items.Fertilizer, needed_fertilizer)
+        if get_pos_x() == 0:
+            if num_items(Items.Carrot_Seed) < needed_seeds_nb:
+                if num_items(Items.Hay) < needed_seeds_nb:
+                    hay_loop()
+                elif num_items(Items.Wood) < needed_seeds_nb:
+                    wood_loop()
+                else:
+                    trade(Items.Carrot_Seed, needed_seeds_nb)
+
+            if num_items(Items.Fertilizer) < needed_fertilizer:
+                if 10 * num_items(Items.Pumpkin) < needed_fertilizer:
+                    pumpkin_loop()
+                trade(Items.Fertilizer, needed_fertilizer)
 
         for i in range(get_world_size()):
             if can_harvest():
