d67bbde596bbd9d110c2f5546a7f9b9047c4a53d
Circus-Pygargus <richard.meuret@laposte.net>
2024-07-04 23:50:20 +0200

    improve carrot loop by using fertilizer

--- carrot_loop.py
+++ carrot_loop.py
@@ -4,6 +4,7 @@ def carrot_loop():
     current_loop = 0
     wanted_loops = get_world_size()**2
     needed_seeds_nb = get_world_size()**2
+    needed_fertilizer = get_world_size()**2
 
     while current_loop <= wanted_loops:
         if get_pos_x() == 0 and num_items(Items.Carrot_Seed) < needed_seeds_nb:
@@ -13,15 +14,21 @@ def carrot_loop():
                 wood_loop()
             else:
                 trade(Items.Carrot_Seed, needed_seeds_nb)
+        if get_pos_x() == 0 and  num_items(Items.Fertilizer) < needed_fertilizer:
+            if 10 * num_items(Items.Pumpkin) < needed_fertilizer:
+                pumpkin_loop()
+            trade(Items.Fertilizer, needed_fertilizer)
 
         for i in range(get_world_size()):
             if can_harvest():
                 harvest()
+                handle_water()
 
             if current_loop == 0:
                 handle_ground(Grounds.Soil)
 
             plant(Entities.Carrots)
+            use_item(Items.Fertilizer)
             handle_water()
 
             move(North)
